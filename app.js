4b25c09b4ba858ed43eb8dccecd80f7011df671c
Abhishek Vaishnav <abhishek777vaishnav@gmail.com>
2021-05-14 11:38:00 +0530

    middleware functioning(basic+implementation approaches)

--- app.js
+++ app.js
@@ -1,56 +1,50 @@
 const express=require('express')
-const { escapeRegExp } = require('lodash')
 const app=express()
-const {products}=require('./data')
 
-app.get('/',(req,res)=>{
-    res.send('<h1>Home page</h1><a href="/api/products">Products</a>')
-})
+//req->middleware->res
 
-
-app.get('/api/products',(req,res)=>{
-    const newProducts=products.map((product)=>{
-        const {id,name,image}=product
-        return {id,name,image}
-    })
-    res.send(newProducts)
-})
-
-app.get('/api/products/:id',(req,res)=>{
-    const singleProduct=products.find((product)=>product.id===Number(req.params.id))
-    if(!singleProduct){
-        res.status(404).send("Products does not exist")
-    }
-    res.json(singleProduct)
-})
+//Method -1
+// app.get('/',(req,res)=>{
+//     const method=req.method
+//     const url=req.url
+//     const time=new Date().getFullYear()
+//     console.log(method,url,time);
+//     res.send('home')
+// })
 
 
-//for queries like: 
-//http://localhost:5000/api/v1/query?search=a&limit=2
+
+//Method-2
+// const logger=(req,res,next)=>{
+//     const method=req.method
+//     const url=req.url
+//     const time=new Date().getFullYear()
+//     console.log(method,url,time)
+//     next()
+// }
+// app.get('/',logger,(req,res)=>{
+//     res.send('home');
+// })
+//Method-2 end
+
+
+
+//Method-3
+const logger=require('./Middleware/logger')
+
+//set middleware
+app.use(logger)
 /*
-Make sure you always "return" response object,when we have conditional rendering
+if it is app.use('/api',logger)
+to work it on those specified path
 */
-app.get('/api/v1/query',(req,res)=>{
-
-    const {search,limit}=req.query
-    let sortedProducts=[...products]
-    
-
-    if(search){
-        sortedProducts=sortedProducts.filter((product)=>{
-            return product.name.startsWith(search)
-        })
-    }
-    if(limit){
-        sortedProducts=sortedProducts.slice(0,Number(limit))
-    }
-    if(!sortedProducts.length){
-        return res.status(404).json({verdict:"fail"})
-    }
-    res.status(200).json(sortedProducts)
-    // res.send("Hello world")
-
+app.get('/',(req,res)=>{
+    res.send("home")
 })
+//Method-3 end
+
+
+
 
 app.listen(5000,()=>{
     console.log("server is listening on port no 5000");
